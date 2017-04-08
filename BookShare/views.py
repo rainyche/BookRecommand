2d3769d3295a4e5fb078d34d2fb52910e5716d3e
Ivy Ma <ivyma@dhcp-10-105-76-136.wireless.northwestern.private>
2017-04-08 04:55:41 -0500

    wrote views

--- BookShare/views.py
+++ BookShare/views.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'BookShare/home.html', {})
+
+def eachuser(request, pk):
+    user = UserProfile.objects.filter(id = pk)
+    return render(request, 'BookShare/user_profile.html', {"user" = user})
+
+def eachbook(request, pk):
+    book = Book.objects.filter(id = pk)
+    return render(request, 'BookShare/review.html', {"book" = book})
+
+def recommendations(request, pk):
+    user = UserProfile.objects.filter(id = pk)
+    booklist = recommendations.recommend(user, Book)
+    return render(request, 'blog/display_book.html', {"books" = booklist})
